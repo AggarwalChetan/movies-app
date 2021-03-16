cd4452ecd936ab425032a050fea9b37cae05f73d
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-16 22:35:23 +0530

    cursor added

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,11 +5,11 @@ import MoviesOverview from './MoviesOverview';
 
 const Movie = ({ title, poster_path, overview, vote_average, release_date, vote_count}) => {
     return (
-        <div className="movie">
+        <button className="movie" onClick={console.log("hey")}>
             <MoviesPoster poster_path={poster_path} title={title}/>  
             <MoviesInfo title={title} vote_average={vote_average}/>
             <MoviesOverview overview={overview} release_date={release_date} vote_count={vote_count}/>
-        </div>
+        </button>
     );
 };
 
