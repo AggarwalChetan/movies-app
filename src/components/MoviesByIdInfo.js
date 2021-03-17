3c7ad8c43ec912db22fa95a06eefaa0c0c836d5d
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-17 20:32:59 +0530

    search api by id

--- src/components/MoviesByIdInfo.js
+++ src/components/MoviesByIdInfo.js
@@ -1,10 +1,17 @@
 function MoviesByIdInfo(props){
 
+    // let moviesById = {};
+    // const SEARCH_MOVIES_API = `http://api.themoviedb.org/3/movie/${props.id}?api_key=61cec280feda32b806f9f3187c008707`;
     
+    // fetch(SEARCH_MOVIES_API)
+    // .then(res => res.json())
+    // .then(data => {
+    //     moviesById = data;
+    //     console.log(moviesById.overview);
+    // });
 
-
-
-    return(<div>{props.id}</div>)
+    // console.log(moviesById)
+    return(<div className="temp">{props.id}</div>);
 }
 
 export default MoviesByIdInfo;
