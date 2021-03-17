a3852f1382d304f0eb3bbbd947625896e2d2e0da
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-17 14:05:53 +0530

    Rate Button added

--- src/components/MoviesOverview.js
+++ src/components/MoviesOverview.js
@@ -3,8 +3,9 @@ function MoviesOverview(props) {
         <div className="movie-over">
             <h2>Overview</h2>
             <h4>{props.release_date}</h4>
-            <p>{props.overview}</p>
+            <p>{props.overview}</p>            
             <h5>Total Vote Count - {props.vote_count}</h5>
+            <button className="voteButton">Want to Rate?</button>
         </div>
     );
 }
