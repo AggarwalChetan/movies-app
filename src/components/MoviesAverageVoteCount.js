cc5166326d3256931609eb16252b7ea24ce06914
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-17 13:06:09 +0530

    move function outside

--- src/components/MoviesAverageVoteCount.js
+++ src/components/MoviesAverageVoteCount.js
@@ -1,14 +1,14 @@
-function MoviesAverageVoteCount(props){
-    const setVoteClass = (vote) => {
-        if(vote >= 8){
-            return 'green';
-        }else if(vote >= 5){
-            return 'yellow';
-        }else{
-            return 'red';
-        }
-    };
+const setVoteClass = (vote) => {
+    if(vote >= 8){
+        return 'green';
+    }else if(vote >= 5){
+        return 'yellow';
+    }else{
+        return 'red';
+    }
+};
 
+function MoviesAverageVoteCount(props){
     return(
         <span className={`tag ${setVoteClass(props.vote_average)}`}>{props.vote_average}</span>
     );
