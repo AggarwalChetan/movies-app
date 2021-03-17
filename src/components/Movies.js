244b81cd38922933c86ca725f00cefd729752702
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-17 11:05:21 +0530

    Overview Fix

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,7 +5,7 @@ import MoviesOverview from './MoviesOverview';
 
 const Movie = ({ title, poster_path, overview, vote_average, release_date, vote_count}) => {
     return (
-        <div className="movie" onClick={console.log("hey")}>
+        <div className="movie">
             <MoviesPoster poster_path={poster_path} title={title}/>  
             <MoviesInfo title={title} vote_average={vote_average}/>
             <MoviesOverview overview={overview} release_date={release_date} vote_count={vote_count}/>
