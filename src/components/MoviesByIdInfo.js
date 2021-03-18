89724508ff1cace515f6189f468c28000dde284a
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-18 16:58:45 +0530

    Movies Info added

--- src/components/MoviesByIdInfo.js
+++ src/components/MoviesByIdInfo.js
@@ -1,10 +1,12 @@
 
 import React from 'react';
+import Movie from './Movies';
+import MoviesPoster from './MoviesPoster';
 
 class MoviesByIdInfo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { movies : {}, id : 522938};
+    this.state = { movies : [], id : 522938};
   };
 
   // for home page
@@ -23,12 +25,21 @@ class MoviesByIdInfo extends React.Component {
   render() {
     return (
       <>
-      
-      <div>{this.state.movies.overview}</div>
+      {/* <div className="movie-container"> */}
+      {/* <div>{this.state.movies.overview}</div>
       <div>{this.state.movies.vote_average}</div>
       <div>{this.state.movies.vote_count}</div>
       <div>{this.state.movies.tagline}</div>
-      <div>{this.state.movies.title}</div>
+      <div>{this.state.movies.title}</div> */}
+      <div className="movieByIdContainer">
+      <div className="movieById">
+          <MoviesPoster poster_path={this.state.movies.poster_path} title={this.state.movies.title} />
+      </div>
+      <div>{this.state.movies.vote_count}</div>
+      </div>
+      
+{/*       
+      </div> */}
       </>
     );
   };
